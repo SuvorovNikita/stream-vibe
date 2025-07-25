133b624ecbea41cb0456ee9b92b311901bda8959
NS <nikit.suvorov@gmail.com>
2025-07-25 22:04:45 +0300

    Refactor index page to use metadata for title and correct SCSS mixin typo

---
+++ src/layouts/Header/Header.jsx
@@ -0,0 +1,14 @@
+import Logo from "@/components/Logo"
+import "./Header.scss"
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__inner container">
+        <Logo loading="eager" />
+      </div>
+    </header>
+  )
+}
+
+export default Header
