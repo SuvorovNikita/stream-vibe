43a88be2948ab3a5079333dd43aba929d39c23a0
NS <nikit.suvorov@gmail.com>
2025-07-27 23:16:09 +0300

    Implement Footer component with structured menu and social links; update global styles for consistent layout and remove unused heading from index page

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -1,5 +1,53 @@
+import "./Footer.scss"
+
 const Footer = () => {
-    return <footer>FOOTER</footer>
+  const menuItems = [
+    {
+      title: "Home",
+      links: ["Categories", "Devices", "Pricing", "FAQ"],
+    },
+    {
+      title: "Movies",
+      links: ["Gernes", "Trending", "New Release", "Popular"],
+    },
+    {
+      title: "Shows",
+      links: ["Gernes", "Trending", "New Release", "Popular"],
+    },
+    {
+      title: "Support",
+      links: ["Contact Us"],
+    },
+    {
+      title: "Subscription",
+      links: ["Plans", "Features"],
+    },
+    {
+      title: "Connect With Us",
+      socialLinks: [
+        { label: "Facebook", iconName: "facebook" },
+        { label: "Twitter", iconName: "twitter" },
+        { label: "LinkedIn", iconName: "linked-in" },
+      ],
+    },
+  ]
+
+  return (
+    <footer className="footer">
+      <div className="footer__inner container">
+        <nav className="footer__menu">
+          {menuItems.map(({ title, links, socialLinks }, index) => (
+            <div className="footer__menu-column" key={index}>
+              <a className="footer__menu-title h6" href="/">
+                {title}
+              </a>
+            </div>
+          ))}
+        </nav>
+        <div className="footer__extra"></div>
+      </div>
+    </footer>
+  )
 }
 
-export default Footer;
+export default Footer
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,7 +5,7 @@ export const metadata = {
 export default function () {
   return (
     <>
-      <h1>Главная</h1>
+   
     </>
   )
 }
