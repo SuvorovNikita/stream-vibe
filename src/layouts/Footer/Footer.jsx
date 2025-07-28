5e023b99162339b3ef3eb692d595cb98285ca3ff
NS <nikit.suvorov@gmail.com>
2025-07-28 22:18:57 +0300

    Remove LinkedIn icon SVG; enhance Button component to support fill icons; update Footer layout with social links and extra links; improve global styles for scrollbar handling.

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -1,3 +1,4 @@
+import Socials from "@/components/Socials"
 import "./Footer.scss"
 
 const Footer = () => {
@@ -25,12 +26,23 @@ const Footer = () => {
     {
       title: "Connect With Us",
       socialLinks: [
-        { label: "Facebook", iconName: "facebook" },
-        { label: "Twitter", iconName: "twitter" },
-        { label: "LinkedIn", iconName: "linked-in" },
+        {
+          label: "Facebook",
+          iconName: "facebook",
+        },
+        {
+          label: "Twitter",
+          iconName: "twitter",
+        },
+        {
+          label: "LinkedIn",
+          iconName: "linked-in",
+        },
       ],
     },
   ]
+
+  const extraLinks = ["Terms of Use", "Privacy Policy", "Cookie Policy"]
 
   return (
     <footer className="footer">
@@ -41,10 +53,35 @@ const Footer = () => {
               <a className="footer__menu-title h6" href="/">
                 {title}
               </a>
+              {links?.length > 0 && (
+                <ul className="footer__menu-list">
+                  {links.map((link, index) => (
+                    <li className="footer__menu-item" key={index}>
+                      <a className="footer__menu-link" href="/">
+                        {link}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              )}
+              {socialLinks?.length > 0 && (
+                <Socials className="footer__soc1als" links={socialLinks} />
+              )}
             </div>
           ))}
         </nav>
-        <div className="footer__extra"></div>
+        <div className="footer__extra">
+          <p className="footer__copyright">
+            @<time dateTime="2025">2025</time> streamvib, All Rights Reserved
+          </p>
+          <div className="footer__extra-links">
+            {extraLinks.map((link, index) => (
+              <a className="footer__extra-link" href="/" key={index}>
+                {link}
+              </a>
+            ))}
+          </div>
+        </div>
       </div>
     </footer>
   )
