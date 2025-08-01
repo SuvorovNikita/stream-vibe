08a24f1ac8162661c0baaba56208748c884fe7d0
NS <nikit.suvorov@gmail.com>
2025-08-01 14:32:38 +0300

    Integrate Slider component into Categories section for enhanced navigation; replace action buttons with SliderNavigation for improved user interaction and layout consistency.

--- src/sections/Categories/Categories.jsx
+++ src/sections/Categories/Categories.jsx
@@ -1,16 +1,19 @@
 import Section from "@/layouts/Section"
 import "./Categories.scss"
 import CategoryCard from "@/components/CategoryCard"
+import Slider from "@/components/Slider"
+import SliderNavigation from "@/components/Slider/components/SliderNavigation"
 
 const Categories = () => {
+  const sliderNavigationId = "categories-slider-navigation"
   const categoryItems = [
     {
       title: "Action",
       images: [
-        '/src/assets/images/categories/action/1.jpg',
-        '/src/assets/images/categories/action/2.jpg',
-        '/src/assets/images/categories/action/3.jpg',
-        '/src/assets/images/categories/action/4.jpg',
+        "/src/assets/images/categories/action/1.jpg",
+        "/src/assets/images/categories/action/2.jpg",
+        "/src/assets/images/categories/action/3.jpg",
+        "/src/assets/images/categories/action/4.jpg",
       ],
     },
     {
@@ -56,17 +59,14 @@ const Categories = () => {
       title="Explore our wide variety of categories"
       titleId="categories-title"
       description="Whether you're looking for a comedy to make you laugh, a drama to make you think, or a documentary to learn something new"
-      actions={
-        <div>
-          <button>назад</button>
-          <button>вперед</button>
-        </div>
-      }
+      actions={<SliderNavigation mode="tile" id={sliderNavigationId} />}
       isActionsHiddenOnMobile
     >
-      {categoryItems.map((categoryItem, index) => (
-        <CategoryCard {...categoryItem} key={index} />
-      ))}
+      <Slider navigationTargetElementId={sliderNavigationId}>
+        {categoryItems.map((categoryItem, index) => (
+          <CategoryCard {...categoryItem} key={index} />
+        ))}
+      </Slider>
     </Section>
   )
 }
